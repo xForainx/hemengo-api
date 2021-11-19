4612525ca410c4a2e107a96f4ccd5e8297111586
hyperdestru <slotuser@gmail.com>
2021-11-19 15:34:57 +0100

    Change force option to alter.

--- db.config.js
+++ db.config.js
@@ -15,7 +15,7 @@ let sequelize = new Sequelize(
 )
 
 // Models synchronization
-sequelize.sync({ force: true }, err => {
+sequelize.sync({ alter: true }, err => {
     console.log("Database sync error", err)
 })
 
