5c400e79fb60b39abbc784a53424994946a77d9e
hyperdestru <slotuser@gmail.com>
2022-01-14 17:18:32 +0100

    Install axios to do requests in specs, write first almost useful spec about order (unauthorized cause not logged in).

--- spec/order.spec.js
+++ spec/order.spec.js
@@ -1,6 +1,15 @@
-describe("A suite", function () {
-    it("contains spec with an expectation", function () {
-        expect(true).toBe(true);
-    });
-});
+const axios = require("axios")
 
+describe("Order", function () {
+    const endpoint = "http://localhost:3000/order"
+
+    describe("GET order without previous login", function () {
+        it("returns Unauthorized as a response", function () {
+            axios.get(endpoint).then(res => {
+                // do something or only use catch ?
+            }).catch(err => {
+                expect(err.response.status).toBe(401)
+            })
+        })
+    })
+})
