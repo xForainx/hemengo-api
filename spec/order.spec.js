9f8594814ce02af35fd84880de9ff37a9ad45480
hyperdestru <slotuser@gmail.com>
2022-01-15 10:05:51 +0100

    Jasmine testing - Delete then() clause on it callback, only catch() clause.

--- spec/order.spec.js
+++ spec/order.spec.js
@@ -4,10 +4,8 @@ describe("Order", function () {
     const endpoint = "http://localhost:3000/order"
 
     describe("GET order without previous login", function () {
-        it("returns Unauthorized as a response", function () {
-            axios.get(endpoint).then(res => {
-                // do something or only use catch ?
-            }).catch(err => {
+        it("returns Unauthorized response", function () {
+            axios.get(endpoint).catch(err => {
                 expect(err.response.status).toBe(401)
             })
         })
