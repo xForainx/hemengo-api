6418b52cd6f194f69c2f9e0b496b96fc064e4eeb
hyperdestru <slotuser@gmail.com>
2022-03-08 15:00:04 +0100

    Suppr. mock test.

--- spec/util.spec.js
+++ spec/util.spec.js
@@ -5,9 +5,5 @@ describe("Util", function () {
         it("returns true for today's date", function () {
             expect(util.isToday(Date.now())).toBeTrue()
         })
-
-        it("mock testing -- it returns 42", function () {
-            expect(util.isToday(Date.now())).toBeGreaterThanOrEqual(42)
-        })
     })
 })
