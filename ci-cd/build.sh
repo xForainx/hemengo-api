acc7f2b11a6f4a769152c580b22cd587300f12d4
hyperdestru <slotuser@gmail.com>
2022-03-16 11:29:02 +0100

    Minor changes in ci scripts.

--- ci-cd/build.sh
+++ ci-cd/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 npm install
+# do this because apidoc is globaly installed, so not in package.json
 npm install apidoc
 npm run doc
--- ci-cd/deploy.sh
+++ ci-cd/deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-npm run start
+#npm run start
+echo "Deployed"
